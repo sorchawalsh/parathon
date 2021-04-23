d0c000b5493264045d3fb0ee432365eaa24e8c72
Sorcha Walsh <33891407+sorchawalsh@users.noreply.github.com>
2021-04-23 15:48:33 +0200

    fixed some underlying issues in the structure of the parathon function

--- parathon/__init__.py
+++ parathon/__init__.py
@@ -1,6 +1,6 @@
 import re
 import json
-
+import csv
 
 class Detect:
     def __init__(self, file, language=None):
@@ -18,13 +18,36 @@ class Detect:
             start = position
             position = position + len(token)
             end = position
+            matches_for_token = list()
             for key in cue_dictionary:
                 if re.search(key, token):
-                    output.append((token, cue_dictionary[key], start, end))
-                else:
-                    output.append((token, start, end))
+                    matches_for_token.append([token, cue_dictionary[key], start, end])
+
+            if len(matches_for_token)>1:
+                ftf_properties=list()
+                cmc_properties=list()
+                for match in matches_for_token:
+                    ftf_properties.append(match[1][0])
+                    cmc_properties.append(match[1][1])
+            elif len(matches_for_token) == 1:
+                ftf_properties=matches_for_token[0][1][0]
+                cmc_properties=matches_for_token[0][1][1]
+            elif (token, "\t", "\t", start, end) not in output:
+                ftf_properties="\t"
+                cmc_properties="\t"
+            output.append((token, ftf_properties, cmc_properties, start, end))
         return output
+
+    def csvify(self, parathon, filename):
+        header=["token", "ftf", "cmc", "start", "end"]
+        with open(filename+".csv", "w", newline="\n", encoding="utf-8") as csvfile:
+            filewriter=csv.writer(csvfile, delimiter="\t", quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            filewriter.writerow(header)
+            for item in parathon:
+                filewriter.writerow(item)
+        csvfile.close()
 
 
 test=Detect("input_test.txt")
-print(test.parathon())
+test.csvify(test.parathon(), "testcsv")
+
