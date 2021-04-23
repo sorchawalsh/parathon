335401c63e5281af5f5ce665044bc6fdf67170fa
Sorcha Walsh <33891407+sorchawalsh@users.noreply.github.com>
2021-04-23 16:04:33 +0200

    added csvify and xmlify
    both to be tested thoroughly

--- parathon/__init__.py
+++ parathon/__init__.py
@@ -32,22 +32,39 @@ class Detect:
             elif len(matches_for_token) == 1:
                 ftf_properties=matches_for_token[0][1][0]
                 cmc_properties=matches_for_token[0][1][1]
-            elif (token, "\t", "\t", start, end) not in output:
-                ftf_properties="\t"
-                cmc_properties="\t"
+            elif (token, " ", " ", start, end) not in output:
+                ftf_properties=" "
+                cmc_properties=" "
             output.append((token, ftf_properties, cmc_properties, start, end))
         return output
 
     def csvify(self, parathon, filename):
         header=["token", "ftf", "cmc", "start", "end"]
         with open(filename+".csv", "w", newline="\n", encoding="utf-8") as csvfile:
-            filewriter=csv.writer(csvfile, delimiter="\t", quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            filewriter=csv.writer(csvfile, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL)
             filewriter.writerow(header)
             for item in parathon:
                 filewriter.writerow(item)
         csvfile.close()
 
+    def xmlify(self, parathon, filename):
+        xml_file = open(filename+".xml", "w", encoding="utf8")
+        output_str_xml = '<?xml version="1.0" encoding="UTF-8"?>\n\t<input>'
+        xml_output=list()
+        for token in parathon:
+            print(token[0])
+            #output_str_xml = output_str_xml + str(token)
+            if token[1]!=" ":
+                token_info_xml='<cue ftf="'+str(token[1])+'" cmc="'+str(token[2])+'">'+str(token[0])+'</cue>'
+                xml_output.append(token_info_xml)
+            else:
+                xml_output.append(token[0])
+        for token in xml_output:
+            output_str_xml = output_str_xml + token
+        output_str_xml = output_str_xml + "\n</input>"
+        xml_file.write(output_str_xml)
+        xml_file.close()
 
 test=Detect("input_test.txt")
-test.csvify(test.parathon(), "testcsv")
-
+#test.csvify(test.parathon(), "testcsv")
+test.xmlify(test.parathon(), "testxml")
